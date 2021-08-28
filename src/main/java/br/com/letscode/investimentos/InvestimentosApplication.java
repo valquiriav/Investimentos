81d08ae909c7ea9ae4824f92084fc38b87e1c74c
valquiriav <valvitoli@gmail.com>
2021-08-28 14:32:49 -0300

    Adding H2

--- src/main/java/br/com/letscode/investimentos/InvestimentosApplication.java
+++ src/main/java/br/com/letscode/investimentos/InvestimentosApplication.java
@@ -1,4 +1,4 @@
-package br.com.letscode.vitreo;
+package br.com.letscode.investimentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/br/com/letscode/vitreo/ClienteTests/ClienteServiceTests.java
@@ -0,0 +1,4 @@
+package br.com.letscode.vitreo.ClienteTests;
+
+public class ClienteServiceTests {
+}
